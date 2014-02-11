3999b2819729b53ba324b67601deb9449bbc18ac
Matthias Wichert <matthias.wichert@gmail.com>
2014-02-12 00:23:57 +0100

    Set up Zend framework and created first "Hello World"

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', true);
+date_default_timezone_set('Europe/Berlin');
+
+$rootDir = dirname(dirname(__FILE__));
+set_include_path($rootDir . '/library' . PATH_SEPARATOR . get_include_path());
+
+require_once('Zend/Loader.php');
+Zend_Loader::loadClass('Zend_Debug');
+Zend_Loader::loadClass('Zend_Controller_Front');
+
+// set up controller
+
+$frontController = Zend_Controller_Front::getInstance();
+$frontController->setControllerDirectory('../application/controllers');
+
+// run!
+$frontController->dispatch();
